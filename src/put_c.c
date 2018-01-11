fadd33decc8c1c703f3f10805817b583b841bd9a
vbrazas <appleitunesstore11@gmail.com>
2018-01-11 21:34:06 +0200

    add4

--- src/put_c.c
+++ src/put_c.c
@@ -6,7 +6,7 @@
 /*   By: vbrazas <vbrazas@student.unit.ua>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/08 21:52:16 by vbrazas           #+#    #+#             */
-/*   Updated: 2018/01/11 21:29:10 by vbrazas          ###   ########.fr       */
+/*   Updated: 2018/01/11 21:33:23 by vbrazas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,18 +23,15 @@
 
 static	void	help1(int k, unsigned int mask[], unsigned char c[])
 {
-	if (k > 0 && k <= 7)
-		mask[0] += write(1, &c[4], 1);
-	if (k > 7 && k <= 11)
+	if (k > 7 && k <= 11 && MB_CUR_MAX >= 2)
 	{
 		c[1] = (mask[4] << 26) >> 26;
 		c[0] = ((mask[4] >> 6) << 27) >> 27;
 		c[4] = (mask[1] >> 8) | c[0];
 		mask[0] += write(1, &c[4], 1);
 		c[4] = ((mask[1] << 24) >> 24) | c[1];
-		mask[0] += write(1, &c[4], 1);
 	}
-	if (k > 11 && k <= 16)
+	if (k > 11 && k <= 16 && MB_CUR_MAX >= 3)
 	{
 		c[2] = (mask[4] << 26) >> 26;
 		c[1] = ((mask[4] >> 6) << 26) >> 26;
@@ -44,14 +41,13 @@ static	void	help1(int k, unsigned int mask[], unsigned char c[])
 		c[4] = ((mask[2] << 16) >> 24) | c[1];
 		mask[0] += write(1, &c[4], 1);
 		c[4] = ((mask[2] << 24) >> 24) | c[2];
-		mask[0] += write(1, &c[4], 1);
 	}
 }
 
 static	void	print_c(int k, unsigned int mask[], unsigned char c[])
 {
 	help1(k, mask, c);
-	if (k > 16)
+	if (k > 16 && MB_CUR_MAX >= 4)
 	{
 		c[3] = (mask[4] << 26) >> 26;
 		c[2] = ((mask[4] >> 6) << 26) >> 26;
@@ -64,8 +60,8 @@ static	void	print_c(int k, unsigned int mask[], unsigned char c[])
 		c[4] = ((mask[3] << 16) >> 24) | c[2];
 		mask[0] += write(1, &c[4], 1);
 		c[4] = ((mask[3] << 24) >> 24) | c[3];
-		mask[0] += write(1, &c[4], 1);
 	}
+	mask[0] += write(1, &c[4], 1);
 }
 
 static	void	handle_minln(unsigned int k, unsigned int mask[],\
